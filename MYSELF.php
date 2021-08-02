85a27bbcbd9e05ed82a617f885684242df45d814
SE-RO <88310261+SE-RO@users.noreply.github.com>
2021-08-02 22:19:28 +0800

    Add files via upload

---
+++ MYSELF.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>  
+<html>
+<title>myself</title>
+<head>
+
+
+</head>
+<style>
+table {
+    margin:none;
+}
+
+tr {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    background: #FFF;
+    color: green;
+    border-collapse: separate;
+    line-height: 40px;
+    
+}
+
+td {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    
+}
+</style>
+<body>  
+
+
+<?php
+echo "<center>
+	<h1>ABOUT MYSELF<br>
+	-----------------------------</h1>
+	<br><br>
+<h3><table>
+   <tr>
+    <td>NAME :&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+    <td>FREDDIE MARK CABUANG</td>
+   </tr>
+   <tr>
+    <td>ADDRESS :&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+    <td>TAGUIG CITY</td>
+   </tr>
+   <tr>
+    <td>AGE :&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+    <td>21</td>
+   </tr>
+   <tr>
+    <td>COLOR:&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+    <td>GREEN</td>
+   </tr>
+   <tr>
+    <td>FOOD:&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+    <td>CHOCOLATE</td>
+   </tr>
+   <tr>
+    <td>GOAL:&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+    <td>I.T.</td>
+   </tr>
+   <tr>
+    <td>HOBBIES:&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+    <td>MINECRAFT,HOUSE CHORES</td>
+   </tr>
+ </table></h3></center>";
+?>
+
+</body>
+</html>
